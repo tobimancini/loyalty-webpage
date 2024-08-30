ee65df48a6b77c279748e9db120b7bd4b8bf63ea
Tobias Mancini <tobias_mancini@hotmail.com>
2024-08-30 15:41:00 -0300

    fixes

--- src/Componentes/Contacto/Contacto.jsx
+++ src/Componentes/Contacto/Contacto.jsx
@@ -20,35 +20,35 @@ const Contacto = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const response = await fetch('/api/sendEmail', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        });
+      const response = await fetch('/api/sendEmail', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
 
-        if (response.ok) {
-            alert('Correo enviado con éxito.');
-        } else {
-            alert('Hubo un error al enviar el correo.');
-        }
+      if (response.ok) {
+        alert('Correo enviado con éxito.');
+      } else {
+        alert('Hubo un error al enviar el correo.');
+      }
     } catch (error) {
-        console.error('Error enviando correo:', error);
+      console.error('Error enviando correo:', error);
     }
-};
+  };
 
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  
+
   return (
     <Background>
       <div className='upperContainer'>
-          <h3>INFORMACIÓN</h3>
-          <h2>Contactanos Ahora</h2>
-        </div>
+        <h3>INFORMACIÓN</h3>
+        <h2>Contactanos Ahora</h2>
+      </div>
       <div className='contactInfo'>
         <div className='infoItem'>
           <FontAwesomeIcon icon={faMapMarkerAlt} />
@@ -94,6 +94,7 @@ const Contacto = () => {
       <div className='mapContainer'>
         <h3>Nuestra Ubicación</h3>
         <iframe
+          title='location'
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26242.997658300876!2d-58.42406886684696!3d-34.54675904688948!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb6a3e2e4e597%3A0x96db6d53d7b01254!2sAv.%20Del%20Libertador%206810%2C%20C1429BNB%20CABA%2C%20Argentina!5e0!3m2!1sen!2sus!4v1692626093585!5m2!1sen!2sus"
           width="100%" height="450" style={{ border: 0 }} allowFullScreen="" loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"></iframe>
--- src/Componentes/Navbar/Hamburger.jsx
+++ src/Componentes/Navbar/Hamburger.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import HamburgerIcon from "./HamburgerIcon";
 import { Link } from "react-router-dom";
 
